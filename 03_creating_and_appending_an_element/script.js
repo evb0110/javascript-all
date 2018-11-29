6d4b7b229adec7d9c5dd1edd7522fc874f6c5e26
evb0110 <7149553@gmail.com>
2018-11-29 17:50:51 +0300

    create and append an element if event

---
+++ 03_creating_and_appending_an_element/script.js
@@ -0,0 +1,14 @@
+const button = document.getElementsByTagName("button")[0];
+const input = document.getElementsByTagName("input")[0];
+let ol = document.querySelector("ol");
+console.log(ol);
+
+button.addEventListener("click", () => {
+  const item = input.value;
+  input.value = "";
+  input.focus();
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(item));
+  // ol.appendChild(li); // add to the end of the list
+  ol.insertBefore(li, ol.childNodes[0]) // add to the start
+});
