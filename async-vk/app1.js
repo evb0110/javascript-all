bd77f1e30f112312b9a17ef1b2423e9e8fef3ba5
evb0110 <7149553@gmail.com>
2019-01-23 00:09:57 +0300

    created fetch app with retry using async/await

--- async-vk/app1.js
+++ async-vk/app1.js
@@ -1,20 +1,31 @@
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+const fetch = require("node-fetch");
 
+const url =
+  // this side has free API for testing
+  // with nice documentation
+  "https://jsonplaceholder.typicode.com/posts/1";
 
 function makeFailingFetch(errProbability) {
   return function(url) {
     if (Math.random() < errProbability) throw new Error("failed to fetch!");
     return fetch(url);
-  }
+  };
 }
 
-const failingFetch = makeFailingFetch(0.5);
+const failingFetch = makeFailingFetch(0.99);
 
+async function fetchWithRetry(url, numRetries = 0) {
+  let lastError = null;
+  for (let i = 0; i < numRetries; i++) {
+    try {
+      console.log(`...try # ${i + 1}`);
+      const res = await failingFetch(url).then(response => response.json());
+      return console.log(res);
+    } catch (error) {
+      lastError = error;
+    }
+  }
+  throw lastError;
+}
 
-failingFetch(url)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
-  });
+fetchWithRetry(url, 1000);
