69c7dafb740311b227e987348ba71ff5386b9355
evb0110 <7149553@gmail.com>
2019-01-23 22:11:43 +0300

    axios works like a charm!

--- axios/01_getting_started/app.js
+++ axios/01_getting_started/app.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
 axios
-  .get("https://jsonplaceholder.typicode.com/posts")
-  .then(res => console.log(res.data.map((r, i) => `${i}. ${r.title}`)))
+  .get("https://jsonplaceholder.typicode.com/photos")
+  .then(res => console.log(res.data.map((r, i) => `${i}. ${r.url}`)))
   .catch(err => console.error(err));
