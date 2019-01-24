6b03e515ca309436f3d3ac48aadb7f6b6d57a264
evb0110 <7149553@gmail.com>
2019-01-25 01:15:51 +0300

    made mocks

---
+++ jest/03_netology_mocks/src/foreach.test.js
@@ -0,0 +1,30 @@
+import foreach from './foreach.js';
+// const foreach = require('./foreach.js')
+
+const mockCallback = jest.fn(x => 42 + x);
+foreach([0, 1], mockCallback);
+
+test('', () => {
+
+  // The mock function is called twice
+  expect(mockCallback.mock.calls.length).toBe(2);
+  
+});
+
+test('', () => {
+  // The first argument of the first call to the function was 0
+  expect(mockCallback.mock.calls[0][0]).toBe(0);
+});
+  
+
+
+test('', () => {
+  // The first argument of the second call to the function was 1
+  expect(mockCallback.mock.calls[1][0]).toBe(1);
+});
+
+
+test('', () => {
+  // The return value of the first call to the function was 42
+  expect(mockCallback.mock.results[0].value).toBe(42);
+});
