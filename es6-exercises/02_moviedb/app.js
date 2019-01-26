ce2ce51bd41570b99353f82f14c684d4e04f96cd
evb0110 <7149553@gmail.com>
2019-01-27 00:55:16 +0300

    made an axios request

---
+++ es6-exercises/02_moviedb/app.js
@@ -0,0 +1,9 @@
+const axios = require("axios");
+
+const apiKey = `e9ddb24aed6d48c4342303aba5269e28`;
+const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
+const imgUrl = `http://image.tmdb.org/t/p/w300/`;
+
+axios.get(`${apiUrl}russian`).then(val => console.log(val.data));
+
+
