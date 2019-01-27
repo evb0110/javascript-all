5341d68cefa02374ca34df9fbbbe23af3bf1ad7a
evb0110 <7149553@gmail.com>
2019-01-27 13:12:11 +0300

    created a working movie search

--- es6-exercises/02_moviedb/app.js
+++ es6-exercises/02_moviedb/app.js
@@ -1,9 +1,31 @@
-const axios = require("axios");
+// const axios = require("axios");
 
 const apiKey = `e9ddb24aed6d48c4342303aba5269e28`;
 const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
 const imgUrl = `http://image.tmdb.org/t/p/w300/`;
 
-axios.get(`${apiUrl}russian`).then(val => console.log(val.data));
+document.querySelector(".form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const searchPattern = document.querySelector(".movie-title").value;
+  const quieryUrl = `${apiUrl}${searchPattern}`;
+  axios.get(quieryUrl).then(val => {
+    document.querySelector(".movies-list").innerHTML = "";
+    for (record of val.data.results) {
+      console.log(record.title);
+      let node = document.createElement("li");
+      let textnode = document.createTextNode(
+        `${record.title}, 
+        ${record.original_title || ''}, 
+        ${record.release_date.split('-')[0]}`);
+      node.appendChild(textnode);
+
+      document.querySelector(".movies-list").appendChild(node);
+
+    }
+  });
+  console.log(quieryUrl);
+  
+});
 
 
