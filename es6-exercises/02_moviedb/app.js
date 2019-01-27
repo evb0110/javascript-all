ead7b753bc1cbbaac974c327a5330501974c3455
evb0110 <7149553@gmail.com>
2019-01-27 13:27:55 +0300

    added catch

--- es6-exercises/02_moviedb/app.js
+++ es6-exercises/02_moviedb/app.js
@@ -4,28 +4,28 @@ const apiKey = `e9ddb24aed6d48c4342303aba5269e28`;
 const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
 const imgUrl = `http://image.tmdb.org/t/p/w300/`;
 
-document.querySelector(".form").addEventListener("submit", (event) => {
+document.querySelector(".form").addEventListener("submit", event => {
   event.preventDefault();
 
   const searchPattern = document.querySelector(".movie-title").value;
   const quieryUrl = `${apiUrl}${searchPattern}`;
-  axios.get(quieryUrl).then(val => {
-    document.querySelector(".movies-list").innerHTML = "";
-    for (record of val.data.results) {
-      console.log(record.title);
-      let node = document.createElement("li");
-      let textnode = document.createTextNode(
-        `${record.title}, 
-        ${record.original_title || ''}, 
-        ${record.release_date.split('-')[0]}`);
-      node.appendChild(textnode);
+  axios
+    .get(quieryUrl)
+    .then(val => {
+      document.querySelector(".movies-list").innerHTML = "";
+      for (record of val.data.results) {
+        console.log(record.title);
+        let node = document.createElement("li");
+        let textnode = document.createTextNode(
+          `${record.title}, 
+        ${record.original_title || ""}, 
+        ${record.release_date.split("-")[0]}`
+        );
+        node.appendChild(textnode);
 
-      document.querySelector(".movies-list").appendChild(node);
-
-    }
-  });
+        document.querySelector(".movies-list").appendChild(node);
+      }
+    })
+    .catch(err => console.error(err));
   console.log(quieryUrl);
-  
 });
-
-
