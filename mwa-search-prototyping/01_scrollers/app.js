22fcddb000130793f4bc62804eb230745a763464
evb0110 <7149553@gmail.com>
2019-01-28 16:06:32 +0300

    started working on showing/hiding context

--- mwa-search-prototyping/01_scrollers/app.js
+++ mwa-search-prototyping/01_scrollers/app.js
@@ -4,16 +4,23 @@ show.addEventListener('click', () => handleShow(event));
 const hide = document.querySelector('.hide');
 hide.addEventListener('click', () => handleHide(event));
 
+const scroll = document.querySelector('.scroll');
+scroll.addEventListener('click', () => handleScroll(event));
+
 const right = document.querySelector('.outer2');
-console.log(right);
 
 function handleShow(event) {
-  console.log(event);
   right.classList.remove("none");
   right.classList.add("block");
 }
 function handleHide(event) {
-  console.log(event);
   right.classList.remove("block");
   right.classList.add("none");
+}
+
+function handleScroll(event) {
+  console.log(event);
+  var myElement = document.getElementById('blue');
+  var topPos = myElement.offsetTop;
+  right.scrollTop = topPos - 200;
 }
