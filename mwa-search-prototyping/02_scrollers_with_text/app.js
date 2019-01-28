63ca2d27f8f428f92d4101a897442f374e683f0f
evb0110 <7149553@gmail.com>
2019-01-28 18:27:26 +0300

    minor changes

--- mwa-search-prototyping/02_scrollers_with_text/app.js
+++ mwa-search-prototyping/02_scrollers_with_text/app.js
@@ -1,37 +1,37 @@
 const show = document.querySelector('.show');
-show.addEventListener('click', () => handleShow(event));
+show.addEventListener('click', () => handleShow());
 
 const hide = document.querySelector('.hide');
-hide.addEventListener('click', () => handleHide(event));
+hide.addEventListener('click', () => handleHide());
 
 const scroll = document.querySelector('.scroll');
-scroll.addEventListener('click', () => handleScroll(event));
+scroll.addEventListener('click', () => handleScroll());
 
 const right = document.querySelector('.outer2');
 const left = document.querySelector('.outer1');
 
 const showContext = document.querySelector('.left-blue .show-context');
-showContext.addEventListener('click', () => handleShowContextClick(event));
+showContext.addEventListener('click', () => handleShowContextClick());
 
 const leftBlue = document.querySelector('.left-blue');
 
-function handleShow(event) {
+function handleShow() {
   right.classList.remove("none");
   right.classList.add("block");
 }
-function handleHide(event) {
+function handleHide() {
   right.classList.remove("block");
   right.classList.add("none");
 }
 
-function handleScroll(event) {
+function handleScroll() {
   const myElement = document.getElementById('blue');
   const topPos = myElement.offsetTop;
   const offsetLeftFromTop = leftBlue.offsetTop - left.scrollTop;
   right.scrollTop = topPos - offsetLeftFromTop;
 }
 
-function handleShowContextClick(event) {
-  handleShow(event);
-  handleScroll(event);
+function handleShowContextClick() {
+  handleShow();
+  handleScroll();
 }
