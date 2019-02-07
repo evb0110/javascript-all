ee8b31ebf067d5bd1aa8e21f16e358fab4f50cf8
evb0110 <7149553@gmail.com>
2019-02-07 16:12:39 +0300

    styling of the header and input fields

---
+++ mwa-search-prototyping/09_page_layout/app.js
@@ -0,0 +1,28 @@
+
+
+const showContext = document.querySelector('.left-blue .show-context');
+showContext.addEventListener('click', () => handleShowContextClick());
+
+
+
+function handleShowContextClick() {
+  handleShow();
+  handleScroll();
+}
+
+const searchButton = document.querySelector('#search');
+const showButton = document.querySelector('#show');
+
+searchButton.addEventListener('click', handleSearch )
+
+function handleSearch(event) {
+  event.preventDefault();
+  console.log('Search button clicked');
+}
+
+showButton.addEventListener('click', handleShow )
+
+function handleShow(event) {
+  event.preventDefault();
+  console.log('Show button clicked');
+}
