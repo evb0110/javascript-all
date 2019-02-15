ffb09c73dcbfd7382fa7b94d112807e2ae1c401b
evb0110 <7149553@gmail.com>
2019-02-16 01:32:13 +0300

    improved reduce body

--- hackerrank/01_birthday_cake_candles/candles.js
+++ hackerrank/01_birthday_cake_candles/candles.js
@@ -7,16 +7,16 @@ const parts = input.split('\n');
 const ar = parts[1].split(' ').map(el => Number(el));
 
 const result = ar.reduce((acc, el) => {
-  let accNew = {...acc}
+  const accNew = {};
   if (el > acc.max) {
     accNew.max = el;
     accNew.count = 1;
   } else if (el == acc.max) {
-    accNew.count++;
+    accNew.count = acc.count + 1;
   }
-  return accNew;
+  return {...acc, ...accNew};
 }, { max: -Infinity, count: 0 });
 
 console.log(result.count)
 
-console.log(ar.filter(el => el == Math.max(...ar)).length);
+// console.log(ar.filter(el => el == Math.max(...ar)).length);
