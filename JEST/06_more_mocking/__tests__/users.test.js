1822ca73568e06a2dc47df6cd61a556e896945ca
evb0110 <7149553@gmail.com>
2019-02-18 00:30:53 +0300

    mocked axios module

---
+++ JEST/06_more_mocking/__tests__/users.test.js
@@ -0,0 +1,16 @@
+// users.test.js
+import axios from "axios";
+import Users from "../src/js/users";
+
+jest.mock("axios");
+
+test("should fetch users", () => {
+  const users = [{ name: "Bob" }];
+  const resp = { data: users };
+  axios.get.mockResolvedValue(resp);
+
+  // or you could use the following depending on your use case:
+  // axios.get.mockImplementation(() => Promise.resolve(resp))
+
+  return Users.all().then(resp => expect(resp).toEqual(users));
+});
