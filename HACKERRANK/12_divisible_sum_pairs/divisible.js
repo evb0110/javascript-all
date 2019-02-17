c92eb975d83e88f05b846a0eba20507de738db79
evb0110 <7149553@gmail.com>
2019-02-17 10:42:52 +0300

    solved 12

---
+++ HACKERRANK/12_divisible_sum_pairs/divisible.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const lines = fs.readFileSync('input.txt', 'utf8').split(/\n/);
+
+function makeNumbers(line) {
+  return line.split(/\s/).map(str => Number(str));
+}
+
+const [[n, k], ar] = lines.map(makeNumbers);
+
+
+function divisibleSumPairs(n, k, ar) {
+  return makePairs(ar).filter((el) => {
+    return sumArray(el) % k == 0;
+  }).length;
+
+  function makePairs(ar) {
+    return ar.reduce((acc, el, i, ar) => {
+      const pairs = ar.slice(i+1).map((elem) => [el, elem]);
+      return [...acc, ...pairs];
+    }, []);
+  }
+  
+  function sumArray(ar) {
+    return ar.reduce((a, b) => a + b);
+  }
+}
+
+console.log(divisibleSumPairs(n, k, ar));
