5e79e67368e25bb703252456832bb8f42a87fef5
evb0110 <7149553@gmail.com>
2019-02-17 10:44:05 +0300

    prettiered

--- HACKERRANK/12_divisible_sum_pairs/divisible.js
+++ HACKERRANK/12_divisible_sum_pairs/divisible.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const lines = fs.readFileSync('input.txt', 'utf8').split(/\n/);
+const lines = fs.readFileSync("input.txt", "utf8").split(/\n/);
 
 function makeNumbers(line) {
   return line.split(/\s/).map(str => Number(str));
@@ -8,19 +8,18 @@ function makeNumbers(line) {
 
 const [[n, k], ar] = lines.map(makeNumbers);
 
-
 function divisibleSumPairs(n, k, ar) {
-  return makePairs(ar).filter((el) => {
+  return makePairs(ar).filter(el => {
     return sumArray(el) % k == 0;
   }).length;
 
   function makePairs(ar) {
     return ar.reduce((acc, el, i, ar) => {
-      const pairs = ar.slice(i+1).map((elem) => [el, elem]);
+      const pairs = ar.slice(i + 1).map(elem => [el, elem]);
       return [...acc, ...pairs];
     }, []);
   }
-  
+
   function sumArray(ar) {
     return ar.reduce((a, b) => a + b);
   }
