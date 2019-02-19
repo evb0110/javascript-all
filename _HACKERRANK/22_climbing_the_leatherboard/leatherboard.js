21c1fc3657237bd80ec0ca0b44f07e9740999797
evb0110 <7149553@gmail.com>
2019-02-19 12:02:56 +0300

    solved, but still timeout in tests

---
+++ _HACKERRANK/22_climbing_the_leatherboard/leatherboard.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const lines = fs.readFileSync('input_large.txt', 'utf8').split(/\n/);
+const lineToNumbers = line => line.split(/\s+/).map(str => Number(str));
+const scores = lineToNumbers(lines[1]);
+const alice = lineToNumbers(lines[3]);
+
+function climbingLeaderboard(scores, alice) {
+  const scoresUnique = [...new Set(scores)];
+  const aliceReversed = alice.reverse();
+  const placesReversed = aliceReversed.reduce((acc, el) => {
+    const currentPlace = acc[acc.length - 1] || 1;
+    for (let i = currentPlace - 1; i < scoresUnique.length; i++) {
+      if (el >= scoresUnique[i]) return [...acc, i+1];
+    }
+    return [...acc, scoresUnique.length + 1];
+  }, [])
+  return placesReversed.reverse();
+}
+
+
+
+console.log(climbingLeaderboard(scores, alice));
