723d3023684b7883cbb6da50d84a5d80048452f7
evb0110 <7149553@gmail.com>
2019-02-22 23:52:31 +0300

    they move and compose

--- _INTER/01_making_window/app.js
+++ _INTER/01_making_window/app.js
@@ -1,17 +1,20 @@
 const state = {
-  count: 0
-}
+  count: 0,
+  zIndexMax: 0
+};
 
-const root = document.querySelector('.root');
+const canvas = document.querySelector('.canvas');
+
+console.log(canvas.offsetLeft, canvas.offsetTop);
 
 const panel = document.createElement('div');
 panel.classList.add('panel');
 
-root.appendChild(panel);
+canvas.appendChild(panel);
 
 const windows = document.createElement('div');
 windows.classList.add('windows');
-root.appendChild(windows);
+canvas.appendChild(windows);
 
 const makeWindowButton = document.createElement('div');
 makeWindowButton.classList.add('make-window');
@@ -22,34 +25,34 @@ panel.appendChild(makeWindowButton);
 
 makeWindowButton.addEventListener('click', makeNewWindow);
 
-
 function makeNewWindow() {
   state.count++;
 
+
   const newWindow = document.createElement('div');
   newWindow.classList.add('window');
+  state.zIndexMax++;
+  newWindow.style.zIndex = state.zIndexMax;
   const windowPanel = document.createElement('div');
   windowPanel.classList.add('window-panel');
-  
+
   const windowName = document.createElement('div');
   const windowNameText = document.createTextNode(`Window ${state.count}`);
   windowName.appendChild(windowNameText);
-  windowName.classList.add('window-name')
+  windowName.classList.add('window-name');
   windowPanel.appendChild(windowName);
 
   const minimizeButton = document.createElement('div');
-  minimizeButton.classList.add('minimize-button')
+  minimizeButton.classList.add('minimize-button');
   const minimizeText = document.createTextNode('_');
   minimizeButton.appendChild(minimizeText);
   windowPanel.appendChild(minimizeButton);
-  
+
   const closeButton = document.createElement('div');
-  closeButton.classList.add('close-button')
+  closeButton.classList.add('close-button');
   const closeText = document.createTextNode('x');
   closeButton.appendChild(closeText);
   windowPanel.appendChild(closeButton);
-
-
 
   newWindow.appendChild(windowPanel);
   windows.appendChild(newWindow);
@@ -63,8 +66,42 @@ function makeNewWindow() {
   closeButton.addEventListener('click', () => {
     newWindow.remove();
     miniName.remove();
-  })
-  
+  });
 
-};
+  minimizeButton.addEventListener('click', () => {
 
+  });
+
+  windowPanel.onmousedown = function(event) {
+    const offsetX = event.pageX - newWindow.offsetLeft;
+    const offsetY = event.pageY - newWindow.offsetTop;
+
+    function moveAt(pageX, pageY) {
+      newWindow.style.left = pageX - offsetX + 'px';
+      newWindow.style.top = pageY - offsetY + 'px';
+    }
+
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    document.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      windowPanel.onmouseup = null;
+    };
+
+    windowPanel.ondragstart = function() {
+      return false;
+    };
+  };
+
+  newWindow.addEventListener('mousedown', handleZIndex);
+
+  function handleZIndex() {
+    state.zIndexMax++;
+    newWindow.style.zIndex = state.zIndexMax;
+  }
+
+}
