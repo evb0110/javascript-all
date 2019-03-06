95468964ffc4ad7eb96700d931d477aef670795a
evb0110 <7149553@gmail.com>
2019-03-06 09:31:08 +0300

    process

---
+++ _ALGORITHMS/07_sliding_window/minSubArrayLen.js
@@ -0,0 +1,13 @@
+function minSubarrayLen(arr, num) {
+  if (!arr.length) return null;
+  const min = +Infinity;
+  const minLength = 0;
+  const start = 0;
+  const end = 0;
+  const sum = arr[start];
+  while (start < arr.length - 1) {
+    if (sum > num) {
+
+    }
+  }
+}
