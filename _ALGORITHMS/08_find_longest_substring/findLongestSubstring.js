ea3ea6eaa9439794a0348ac074a369293d18b269
evb0110 <7149553@gmail.com>
2019-03-06 23:27:14 +0300

    misc

--- _ALGORITHMS/08_find_longest_substring/findLongestSubstring.js
+++ _ALGORITHMS/08_find_longest_substring/findLongestSubstring.js
@@ -8,9 +8,7 @@ function findLongestSubstring(str) {
     if (seen[char]) {
       start = Math.max(start, seen[char]);
     }
-    // index - beginning of substring + 1 (to include current in count)
     longest = Math.max(longest, i - start + 1);
-    // store the index of the next char so as to not double count
     seen[char] = i + 1;
   }
   return longest;
