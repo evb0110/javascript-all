996dbc7fdf076bfd2f945339c85a5d2c2ff3c98b
evb0110 <7149553@gmail.com>
2019-03-06 15:16:09 +0300

    solved with generator

--- _ALGORITHMS/07_sliding_window/minSubArrayLen.js
+++ _ALGORITHMS/07_sliding_window/minSubArrayLen.js
@@ -1,13 +1,33 @@
-function minSubarrayLen(arr, num) {
-  if (!arr.length) return null;
-  const min = +Infinity;
-  const minLength = 0;
-  const start = 0;
-  const end = 0;
-  const sum = arr[start];
-  while (start < arr.length - 1) {
-    if (sum > num) {
+function minSubArrayLen(arr, num) {
+  let minLen = +Infinity;
+  for (const chunk of makeChunkIterator(arr, num)) {
+    if (chunk.sum >= num) {
+      minLen = Math.min(minLen, chunk.len);
+    }
+  }
+  if (minLen === Infinity) return 0;
+  return minLen;
+}
 
+function* makeChunkIterator(arr, num) {
+  let left = 0;
+  let right = 0;
+  let sum = arr[left];
+  while (right < arr.length) {
+    const len = right - left + 1;
+    yield {
+      len,
+      sum
+    };
+    if (sum < num) {
+      right++;
+      sum += arr[right];
+    } else {
+      sum -= arr[left];
+      left++;
     }
   }
 }
+
+
+console.log(minSubarrayLen([2, 3, 1, 2, 4, 3], 7))
