975e5a86b46f0170c73cdb6b0c634e2b2a9fe439
evb0110 <7149553@gmail.com>
2019-03-08 11:36:10 +0300

    done

---
+++ _ALGORITHMS/25_binary_search/binary.js
@@ -0,0 +1,21 @@
+function binarySearch(arr, val) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start < end) {
+    const middle = Math.round((end + start) / 2);
+    if (arr[middle] === val) return middle;
+    if (arr[middle] < val) {
+      start = middle;
+    } else {
+      end = middle;
+    }
+  }
+  return -1;
+}
+
+const arr = [1,2,3,4,5,6,7,8,9];
+const val = 7;
+
+const result = binarySearch(arr, val);
+console.log(result);
