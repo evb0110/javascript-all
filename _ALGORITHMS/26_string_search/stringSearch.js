e32a390957c75541d728c1dd64cf4c4171c41265
evb0110 <7149553@gmail.com>
2019-03-08 12:34:08 +0300

    fixed an error with indexing

--- _ALGORITHMS/26_string_search/stringSearch.js
+++ _ALGORITHMS/26_string_search/stringSearch.js
@@ -2,12 +2,12 @@ function stringSearch(long, short) {
   const result = [];
   outer: for (let i = 0; i < long.length - short.length + 1; i++) {
     for (let j = 0; j < short.length; j++) {
-      if (long[i + j - 1] !== short[j]) continue outer;
+      if (long[i + j] !== short[j]) continue outer;
     }
     result.push(i);
   }
-  return result
+  if (result.length) return result;
+  return [-1];
 }
 
-console.log(stringSearch('wahoorohoori', 'hoo'));
-
+console.log(stringSearch('warihoorohoori', 'oo'));
