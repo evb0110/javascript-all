00423a70f2468eeb77d397d3fc6a4265055cb00e
evb0110 <7149553@gmail.com>
2019-03-10 00:48:05 +0300

    optimized

--- _ALGORITHMS/31_merge_arrays/merge.js
+++ _ALGORITHMS/31_merge_arrays/merge.js
@@ -3,13 +3,23 @@ function merge(arr1, arr2) {
   let start2 = 0;
   let result = [];
 
-  while (start1 < arr1.length || start2 < arr2.length) {
-    if ((arr1[start1] || Infinity) < (arr2[start2] || Infinity)) {
+  while (start1 < arr1.length && start2 < arr2.length) {
+    if (arr1[start1] < arr2[start2]) {
       result.push(arr1[start1++]);
     } else {
       result.push(arr2[start2++]);
     }
   }
+  if (start1 == arr1.length) {
+    for (let i = start2; i < arr2.length; i++) {
+      result.push(arr2[i]);
+    }
+  } else if (start2 == arr2.length) {
+    for (let i = start1; i < arr1.length; i++) {
+      result.push(arr1[i]);
+    }
+  }
+
   return result;
 }
 
