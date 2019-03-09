c147214528974ed7b595b6e2ae0af8447088f3cd
evb0110 <7149553@gmail.com>
2019-03-09 11:38:11 +0300

    done

---
+++ _ALGORITHMS/29_selection_sort/selectionSort.js
@@ -0,0 +1,29 @@
+function selectionSort(inputArray) {
+  const arr = [...inputArray];
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    const minIdx = findMinIdx(arr, start, end);
+    swapElems(arr, start, minIdx);
+    start++;
+  }
+  return arr;
+}
+
+function findMinIdx(arr, start, end) {
+  let minIdx = start;
+  let min = arr[minIdx];
+  for (let i = start + 1; i <= end; i++) {
+    if (arr[i] < min) {
+      minIdx = i;
+      min = arr[i];
+    }
+  }
+  return minIdx;
+}
+
+function swapElems(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
+console.log(selectionSort([5, 55, 1, -5, 100, 3, 7, 4, 2]));
