0286b92b9f2114d81970bae665be690816d35e34
evb0110 <7149553@gmail.com>
2019-03-11 00:32:58 +0300

    solved

---
+++ _HACKERRANK/67_grid_search/grid.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const input = fs.readFileSync('input.txt', 'utf8');
+const lines = input.split(/\n/);
+const gridHeight = +lines[0].split(/\s+/)[0];
+const G = lines.slice(1, gridHeight + 1);
+// const patternHeight = +lines[gridHeight + 1].split(/\s+/)[0]
+const P = lines.slice(gridHeight + 2);
+
+// ========================
+function gridSearch(G, P) {
+  const matchesArray = [];
+  for (let i = 0; i < G.length - P.length + 1; i++) {
+    const matches = [];
+    for (let k = 0; k < P.length; k++) {
+      let large = G[i + k];
+      let small = P[k];
+      matches.push(indices(large, small));
+    }
+    if (matches.length) {
+      matchesArray.push(matches);
+    }
+  }
+  return matchesArray.filter(el => goodArray(el) > 0).length > 0 ? 'YES' : 'NO';
+}
+
+function indices(large, small) {
+  const result = [];
+  let idx = 0;
+  while (~(idx = large.indexOf(small, idx))) {
+    result.push(idx++);
+  }
+  return result;
+}
+
+function goodArray(arr) {
+  return arr.reduce((acc, el) => arrIntersection(acc, el)).length; 
+}
+
+function arrIntersection(arr1, arr2) {
+  return arr1.filter(el => arr2.includes(el));
+}
+// ===========================
+
+// console.log(arrIntersection([5, 3, 6, 3], [2, 5, 3, 2, 9]));
+
+console.log(gridSearch(G, P));
+// console.log(goodArray([ [ 1, 3, 6 ], [ 5 ], [ 3, 6, 8 ] ]))
