0ce18f48b873af4cd49f49006aa7ba9aff529148
evb0110 <7149553@gmail.com>
2019-03-10 23:21:06 +0300

    in place

--- _ALGORITHMS/33_quick_sort_in_place/quickSort.js
+++ _ALGORITHMS/33_quick_sort_in_place/quickSort.js
@@ -1,4 +1,34 @@
-function pivot(arr) {
-  let start = 0;
-  let end = arr.length + 1;
+function pivot(arr, start = 0, end = arr.length - 1) {
+  let pivot = arr[start];
+  let swapIdx = start;
+  for (let i = start + 1; i <= end; i++) {
+    if (arr[i] < pivot) {
+      swap(arr, i, ++swapIdx);
+    }
+  }
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+function quickSort(arr, start = 0, end = arr.length - 1) {
+  if (end - start < 1) {
+    console.log(end, start, end - start);
+    return;
+  }
+  const pivotIdx = pivot(arr, start, end);
+  quickSort(arr, start, pivotIdx - 1);
+  quickSort(arr, pivotIdx + 1, end);
+  return arr;
+}
+
+const arr = [5, 5, 10, 8, 3, 4, 6, 4, 7];
+// const arr = [];
+// for (let i = 0; i < 1e5; i++) {
+//   arr.push(Math.random());
+// }
+
+console.log(quickSort(arr));
+
+function swap(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
 }
