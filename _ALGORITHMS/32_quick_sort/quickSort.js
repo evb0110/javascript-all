93e651d92427dd6e9fb9e8d7fbfdc5196a2f6846
evb0110 <7149553@gmail.com>
2019-03-10 22:09:00 +0300

    quick sort

---
+++ _ALGORITHMS/32_quick_sort/quickSort.js
@@ -0,0 +1,28 @@
+function quickSort(arr) {
+  if (arr.length === 0) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const midValue = arr[mid];
+  const left = [];
+  const right = [];
+  const center = [];
+
+  for (const el of arr) {
+    if (el < midValue) {
+      left.push(el);
+    } else if (el === midValue) {
+      center.push(el);
+    } else {
+      right.push(el);
+    }
+  }
+
+  return [...quickSort(left), ...center, ...quickSort(right)];
+}
+
+// const arr = [5, 2, 7, 2, 3, 7, 33, 6, 34, 234, 21];
+const arr = [];
+for (let i = 0; i < 1e7; i++) {
+  arr.push(Math.random());
+}
+const result = quickSort(arr);
+console.log(result);
---
+++ _ALGORITHMS/33_quick_sort_in_place/quickSort.js
@@ -0,0 +1,4 @@
+function pivot(arr) {
+  let start = 0;
+  let end = arr.length + 1;
+}
