f439704de176921a7bc317edb942bb18af374903
evb0110 <7149553@gmail.com>
2019-03-11 21:35:50 +0300

    done

--- _HACKERRANK/68_happy_ladybugs/happy.js
+++ _HACKERRANK/68_happy_ladybugs/happy.js
@@ -3,19 +3,19 @@ const lines = fs.readFileSync('input.txt', 'utf8').split(/\n/);
 const g = +lines[0];
 const b = lines[1];
 
+// ==========================================
 function hasSingulars(str) {
   const histogram = [...str].reduce((acc, el) => {
     acc[el] = ++acc[el] || 1;
     return { ...acc };
   }, {});
-  console.log(histogram);
   return !!Object.entries(histogram)
     .filter(entry => entry[0] !== '_')
     .filter(entry => entry[1] === 1).length;
 }
 
-function isHappy(str) {
-  return [...str].reduce((acc, el, i, arr) => {
+function isHappy(b) {
+  return [...b].reduce((acc, el, i, arr) => {
     if (el === '_') return acc;
     return acc && (el === arr[i - 1] || el === arr[i + 1]);
   }, true);
@@ -23,8 +23,10 @@ function isHappy(str) {
 
 function happyLadybugs(b) {
   if (hasSingulars(b)) return 'NO';
-  if (b.includes('_')) return 'YES';
   if (isHappy(b)) return 'YES';
+  if (b.includes('_')) return 'YES';
+  return 'NO'
 }
+// ==========================================
 
 console.log(happyLadybugs(b));
