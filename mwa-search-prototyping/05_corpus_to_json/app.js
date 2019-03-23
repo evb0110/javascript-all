55398606defa7dfdebdc6cbccaff282c0aed72a4
evb0110 <7149553@gmail.com>
2019-03-23 21:45:12 +0300

    mwa json etc

--- mwa-search-prototyping/05_corpus_to_json/app.js
+++ mwa-search-prototyping/05_corpus_to_json/app.js
@@ -1,7 +1,7 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const directory = "_corpus";
+const directory = '_corpus';
 
 const vols = fs.readdirSync(directory);
 
@@ -11,33 +11,24 @@ const textus = volsTextus.map(proceedVolume);
 const volsVersio = vols.filter(dir => /TRANS/.test(dir));
 const versio = volsVersio.map(proceedVolume);
 
-
 // these loops are ugly, they mutate textus,
-// but they do what they are supposed to and 
+// but they do what they are supposed to and
 // I don't know how to do
-// it in a better way 
+// it in a better way
 for (let i = 0; i < textus.length; i++) {
   const volName = textus[i].volume;
   const volVersioName = addTransToName(volName);
   if (versio[i].volume === volVersioName) {
     for (let j = 0; j < textus[i].texts.length; j++) {
-      let text = textus[i].texts[j]
+      let text = textus[i].texts[j];
       for (let k = 0; k < text.contents.length; k++) {
-        let line = text.contents[k]
-        // try {
-
+        let line = text.contents[k];
           line.versio = versio[i].texts[j].contents[k].textus;
-        // } catch(e) {
-        //   console.log(i, j, k, versio[i].texts[j])
-        // }
       }
     }
   }
 }
-// console.log(JSON.stringify(textus, null, 2));
-
-
-
+console.log(JSON.stringify(textus, null, 2));
 
 // ===== FUNCTIONS =====
 
@@ -45,16 +36,16 @@ function proceedVolume(volume) {
   const volumePath = path.join(directory, volume);
   const texts = fs.readdirSync(volumePath);
   const result = texts.map(textName => proceedText(textName, volumePath));
-  return { volume, texts: result }
+  return { volume, texts: result };
 }
 
 function proceedText(textName, volumePath) {
   const textNumber = getNumber(textName);
   const textPath = path.join(volumePath, textName);
-  const textContents = fs.readFileSync(textPath, "utf8");
-  let lines = textContents.split("\n").filter(line => /\S/.test(line));
+  const textContents = fs.readFileSync(textPath, 'utf8');
+  let lines = textContents.split('\n').filter(line => /\S/.test(line));
   const result = lines.map((line, i) => proceedLine(line, i));
-  return { textNumber, textName, contents: result }
+  return { textNumber, textName, contents: result };
 }
 
 function proceedLine(line, i) {
@@ -63,7 +54,7 @@ function proceedLine(line, i) {
 }
 
 function getNumber(line) {
-  return line.substr(0,3);
+  return line.substr(0, 3);
 }
 
 function addTransToName(str) {
