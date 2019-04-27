589137fa74d3754e80a212ea041e87eb7ee060b8
evb0110 <7149553@gmail.com>
2019-04-27 10:43:11 +0300

    done

---
+++ _HACKERRANK/71_absolute-permutations/permutations.js
@@ -0,0 +1,37 @@
+// [1,n] - range, k - difference
+
+function absolutePermutation(n, k) {
+  if (n % (2 * k)) return [-1];
+  const a = [];
+  for (let i = 0; i < n; i++) {
+    a.push(i + 1);
+  }
+  if (k === 0) return a;
+  const aSplitted = splitArray(a, k);
+  for (let i = 0; i < aSplitted.length; i = i + 2) {
+    [aSplitted[i], aSplitted[i + 1]] = [aSplitted[i + 1], aSplitted[i]];
+  }
+  const result = [];
+  aSplitted.forEach(arr => {
+    arr.forEach(el => result.push(el));
+  });
+  return result;
+}
+
+function splitArray(arr, k) {
+  const result = [];
+  const ln = Math.floor(arr.length / k);
+  for (let i = 0; i < ln; i++) {
+    result[i] = [];
+    for (let j = 0; j < k; j++) {
+      result[i].push(arr[i * k + j]);
+    }
+  }
+  return result;
+}
+
+function swap(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
+console.log(absolutePermutation(86542, 1).join(' ') + '\n');
